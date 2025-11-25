d3634d6b529864a8d94ddfb6df9688c224c5ed3d
SabirHossen339 <sabirshimanto01729@gmail.com>
2025-11-25 19:22:45 +0600

    added Navbar

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  const links = <>
+    <Link to='/'><li className="m-2 text-xl font-semibold">Home</li></Link>
+    <Link to=''><li className="m-2 text-xl font-semibold">Apps</li ></Link>
+    <Link to=''><li className="m-2 text-xl font-semibold">Installation</li ></Link>
+
+  </>
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {links}
+          </ul>
+        </div>
+        <img className='w-[50px]' src="/assets/logo.png" alt="" />
+        <a className="font-bold md:text-2xl lg:text-2xl">Hero.IO</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a href='https://github.com/SabirHossen339/My-Hero-App' className="btn rounded-xl text-white bg-linear-to-l from-purple-400 via-purple-500 to-purple-600"><FaGithub />Contribute</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import { router } from '../components/Route/Route.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 )
